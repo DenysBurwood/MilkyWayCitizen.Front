d00a10d233eb38e0f81d9e62b250b4304e37ab57
Denys Burwood <denys.burwood@hotmail.be>
2026-02-02 09:11:19 +0100

    stary-sky component commented (dependency conflicts)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./components/layout/nav-bar/nav-bar";
-import { NgxStarrySkyComponent } from '@omnedia/ngx-starry-sky';
+//import { NgxStarrySkyComponent } from '@omnedia/ngx-starry-sky';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBar, RouterOutlet, NgxStarrySkyComponent],
+  imports: [NavBar, RouterOutlet/*, NgxStarrySkyComponent*/],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
