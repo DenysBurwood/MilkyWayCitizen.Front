3d9e1ef835aa2faac83ddc482907902aac67c674
Denys Burwood <denys.burwood@hotmail.be>
2026-02-02 11:16:06 +0100

    cleaned dependancies

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./components/layout/nav-bar/nav-bar";
-import { NgxStarrySkyComponent } from '@omnedia/ngx-starry-sky';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBar, RouterOutlet, NgxStarrySkyComponent],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
